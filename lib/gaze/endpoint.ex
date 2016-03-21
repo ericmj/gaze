c0f73cf49977f1ea59c3a1e8a5e3e0681410cd1f
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-03-21 20:56:28 +0100

    Clean up router and endpoint

--- lib/gaze/endpoint.ex
+++ lib/gaze/endpoint.ex
@@ -8,8 +8,8 @@ defmodule Gaze.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :gaze, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: :gaze,
+    only: ~w(css fonts js)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
